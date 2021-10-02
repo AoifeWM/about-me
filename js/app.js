ef21f97f535a3d6ba8090f44f3c636e9d8cce2b7
AoifeWM <aoifewmulligan@gmail.com>
2021-10-02 15:14:24 -0700

    Updated guessing game, added list of favorite animals, added styles for lists

--- js/app.js
+++ js/app.js
@@ -88,11 +88,64 @@ function quiz(name){
             alert('Please answer yes or no.');
         }
     }
+    let chances = 0;
+    let paintYears = 4;
+    for (chances; chances <4; chances++) {
+        let guess = prompt(`How many years has Aoife been painting? You have ${4-chances} guesses left.`);
+        if (isNaN(Math.round(parseInt(guess)))){
+            alert ('Please enter a number!');
+            chances--;
+        } else if (guess > paintYears){
+            alert ('Too high.');
+        } else if (guess < paintYears){
+            alert ('Too low.');
+        } else {
+            alert ('You\'re right! ')
+            chances = 5;
+            score++;
+        }
+    }
+    let spices = ['pepper', 'chili', 'anise', 'nutmeg', 'cinnamon', 'fennel', 'ginger', 'basil', 'mint', 'oregano', 'thyme', 'parsley'];
+    let correctAnswers = [];
+    for (chances = 0; chances < 6; chances++){
+        let guess = prompt(`What are Aoife's favorite herbs and spices? You have ${6-chances} guesses left.`);
+        let correct = false;
+        for(let i = 0; i < spices.length; i++){
+            console.log(`guess: ${guess.toLowerCase()}, index = ${i}, array value = ${spices[i]}, match = ${guess.toLowerCase() === spices[i]}`);
+            if(guess.toLowerCase() === spices[i]) {
+                console.log('match detected.');
+                console.log(`repeat value? ${repeatChecker(i, correctAnswers)}`);
+                if(repeatChecker(i, correctAnswers)){
+                    correct = true;
+                    alert(`You already guessed ${guess}, stop trying to cheat, ${name}!`);
+                }else{
+                    score++;
+                    correct = true;
+                    correctAnswers.push(i);
+                    alert(`That's right! Aoife loves ${guess}!`);
+                }
+            }
+        }
+        if(correct === false){
+            alert(`Sorry, ${guess} isn't one of her favorites :(`);
+        }
+    }
+
+
     alert('Thanks for playing, ' + name + "!");
-    document.write('You scored ' + score + ' out of 5 on the Aoife quiz. ')
-    if(score === 5){
+    document.write('You scored ' + score + ' out of 12 on the Aoife quiz. ')
+    if(score >= 9){
         document.write('Well done, ' + name +'!');
     } else {
         document.write('Retake the quiz if you want to score higher, ' + name + '.');
     }
+}
+
+function repeatChecker(index, correctAnswers){
+    for(let j = 0; j < correctAnswers.length; j++){
+        if(index === correctAnswers[j]){
+            return true;
+        }
+    }
+    return false;
 }
